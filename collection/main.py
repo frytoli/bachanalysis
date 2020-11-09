cd94f7ddf8c8578b9dc94c28ecb9e8daf09bee18
frytoli <omafryt@gmail.com>
2020-11-09 13:43:05 -0600

    debugged/altered multiprocessing logic, created option for data output format, moved sleep to beginning of scraping methods to randomize execution start time, and more appropriate shebang

--- collection/main.py
+++ collection/main.py
@@ -1,4 +1,4 @@
-#!/usr/local/bin/python3
+#!/usr/bin/env python
 
 from multiprocessing import Pool
 from scrapers import *
@@ -17,88 +17,106 @@ MAX_BACHELORETTE_SEASON = 17
 Collect general information from all seasons of The Bachelor
 https://en.wikipedia.org/wiki/The_Bachelor_(American_TV_series)
 '''
-def scrape1():
-    resp = ds1.scrape()
-    # Temp write data to file
-    with open('../data/ds1.json','w') as outjson:
-        outjson.write(resp, outjson, indent=2)
-    # Assume the execution of multiple requests
+def scrape1(to_db=True):
+    # Assume the execution of multiple requests, randomize execution time
     time.sleep(random.uniform(3,8))
+    resp = ds1.scrape()
     return resp
 
 '''
 Collect general information from all seasons of The Bachelorette
 https://en.wikipedia.org/wiki/The_Bachelorette
 '''
-def scrape2():
-    resp = ds2.scrape()
-    # Temp write data to file
-    with open('../data/ds2.json','w') as outjson:
-        json.dump(resp, outjson, indent=2)
-    # Assume the execution of multiple requests
+def scrape2(to_db=True):
+    # Assume the execution of multiple requests, randomize execution time
     time.sleep(random.uniform(3,8))
+    resp = ds2.scrape()
     return resp
 
 '''
 Collect general information about all contestants from a given season or all seasons of The Bachelor
 https://en.wikipedia.org/wiki/The_Bachelor_(season_1)
 '''
-def scrape3(season):
-    resp = ds3.scrape_season(season)
-    # Temp write data to file
-    with open(f'../data/bachelor{season}.json','w') as outjson:
-        json.dump(resp, outjson, indent=2)
-    # Assume the execution of multiple requests
+def scrape3(season, to_db=True):
+    # Assume the execution of multiple requests, randomize execution time
     time.sleep(random.uniform(3,8))
+    resp = ds3.scrape_season(season)
     return resp
 
 '''
 Collect general information about all contestants from a given season or all seasons of The Bachelorette
 https://en.wikipedia.org/wiki/The_Bachelorette_(season_1)
 '''
-def srcape4(season):
-    resp = ds4.scrape_season(season)
-    with open(f'../data/bachelorette{season}.json','w') as outjson:
-        json.dump(resp, outjson, indent=2)
-    # Assume the execution of multiple requests
+def scrape4(season, to_db=True):
+    # Assume the execution of multiple requests, randomize execution time
     time.sleep(random.uniform(3,8))
+    resp = ds4.scrape_season(season)
     return resp
 
 '''
 Collect photos and additional physical information of one Bachelor/Bachelorette cast member or all Bachelor/Bachelorette cast members
 https://bachelor-nation.fandom.com/wiki/Alex_Michel
 '''
-def scrape5(contestant):
-    resp = ds5.scrape_contestant(contestant)
-    with open(f'../data/{contestant.lower()}.json','w') as outjson:
-        json.dump(resp, outjson, indent=2)
-    # Assume the execution of multiple requests
+def scrape5(contestant, to_db=True):
+    # Assume the execution of multiple requests, randomize execution time
     time.sleep(random.uniform(3,8))
+    resp = ds5.scrape_contestant(contestant)
     return resp
 
 def main():
     # Retrieve args
     parser = argparse.ArgumentParser(description='Process some integers.')
     parser.add_argument('scraper', metavar='N', type=int, nargs='+', help='an integer associated with a data source (i.e. 4)')
-    parser.add_argument('--season', dest='season', type=int, nargs='+', action='store', default=[None], help='an integer season (only applicable with data sources 3 and 4) (i.e. 11)')
-    parser.add_argument('--contestant', dest='contestant', type=str, nargs='+', action='store', default=[None], help='a string contestant first and last name separated by "_" (only applicable with data source 5) (i.e. joelle_fletcher)')
+    parser.add_argument('--season', dest='season', type=int, nargs='+', default=[None], help='an integer season (only applicable with data sources 3 and 4) (i.e. 11)')
+    parser.add_argument('--contestant', dest='contestant', type=str, nargs='+', default=[None], help='a string contestant first and last name separated by "_" (only applicable with data source 5) (i.e. joelle_fletcher)')
+    parser.add_argument('--output', dest='output', type=str, choices=['db', 'file'], default='db', help='a string desired output format for collected data: (db or file)')
     args = parser.parse_args()
+
+    # Specify output format
+    to_db = True
+    if args.output == 'file':
+        to_db = False
+        path_to_volume = os.path.join(os.getcwd(), 'data')
 
     # Initialize multiprocessing pool with 5 threads
     pool = Pool(processes=5)
     # Run asyncronous processes
     if 1 in args.scraper:
-        ds1_resp = pool.map_async(scrape1)
+        ds1_data = pool.apply_async(scrape1, kwds={'to_db':to_db})
+        ds1_resp = ds1_data.get()
+        # Write json response to file, if applicable
+        if not to_db:
+            with open(os.path.join(path_to_volume, 'ds1.json'),'w') as outfile:
+                json.dump(ds1_resp, outfile, indent=2)
     if 2 in args.scraper:
-        ds2_resp = pool.map_async(scrape2)
+        ds2_data = pool.apply_async(scrape2, kwds={'to_db':to_db})
+        ds2_resp = ds2_data.get()
+        # Write json response to file, if applicable
+        if not to_db:
+            with open(os.path.join(path_to_volume, 'ds2.json'),'w') as outfile:
+                json.dump(ds2_resp, outfile, indent=2)
     if 3 in args.scraper:
         if args.season == [None]:
-            args.season = list(range(1, MAX_BACHELOR_SEASON+1)) # Hardcoded max episodes
-        ds3_resp = pool.map_async(scrape3, args.season)
+            seasons = list(range(1, MAX_BACHELOR_SEASON+1)) # Hardcoded max episodes
+        else:
+            seasons = args.season
+        ds3_data = pool.apply_async(scrape3, seasons, kwds={'to_db':to_db for s in seasons})
+        ds3_resp = ds3_data.get()
+        # Write json response to file, if applicable
+        if not to_db:
+            with open(os.path.join(path_to_volume, 'ds3.json'),'w') as outfile:
+                json.dump(ds3_resp, outfile, indent=2)
     if 4 in args.scraper:
         if args.season == [None]:
-            args.season = list(range(1, MAX_BACHELORETTE_SEASON+1)) # Hardcoded max episodes
-        ds4_resp = pool.map_async(scrape4, args.season)
+            seasons = list(range(1, MAX_BACHELORETTE_SEASON+1)) # Hardcoded max episodes
+        else:
+            seasons = args.season
+        ds4_data = pool.apply_async(scrape4, seasons, kwds={'to_db':to_db for s in seasons})
+        ds4_resp = ds4_data.get()
+        # Write json response to file, if applicable
+        if not to_db:
+            with open(os.path.join(path_to_volume, 'ds4.json'),'w') as outfile:
+                json.dump(ds4_resp, outfile, indent=2)
     if 5 in args.scraper:
         if args.contestant == [None]:
             # Read-in json files of all previously collected contestants
@@ -113,8 +131,14 @@ def main():
                 if os.path.exists(f'../data/bachelorette{season}.json'):
                     df = pd.read_json(f'../data/bachelorette{season}.json')
                     contestants += [name.strip().replace(' ','_') for name in df['Name'] if ' ' in name.strip()]
-            args.contestant = contestants
-        ds5_resp = pool.map_async(scrape5, args.contestant)
+        else:
+            contestants = args.contestant
+        ds5_data = pool.apply_async(scrape5, contestants, kwds={'to_db':to_db for c in contestants})
+        ds5_resp = ds5_data.get()
+        # Write json response to file, if applicable
+        if not to_db:
+            with open(os.path.join(path_to_volume, 'ds5.json'),'w') as outfile:
+                json.dump(ds5_resp, outfile, indent=2)
 
 
 if __name__ == '__main__':
