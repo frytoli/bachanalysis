e5f4d4597267cbda9968391f1b57787070bccbf4
frytoli <omafryt@gmail.com>
2020-11-10 13:46:44 -0600

    implemented new, more data model-focused database class, removed data flattening method, removed reading from fles, implemented always writing data to database with the option of outputting to a file, implemented optional database table overwriting, writing data to database in multiprocessed scraping methods themselves, and added global databse path var

--- collection/main.py
+++ collection/main.py
@@ -5,14 +5,16 @@ from scrapers import *
 import pandas as pd
 import argparse
 import random
-import sqldb
 import json
 import time
+import db
 import os
 import re
 
 # Global var for path to volume within container
 PATH_TO_VOLUME = os.path.join(os.getcwd(), 'data')
+# Global var for path to database
+PATH_TO_DB = os.path.join(PATH_TO_VOLUME, 'thebach.db')
 # Global compiled regex pattern for Wikipedia references
 REF_PATTERN = re.compile(r'\[.*\]')
 # Global compiled regex pattern for ignoring quoted nicknames
@@ -21,6 +23,16 @@ NN_PATTERN = re.compile(r'\"[a-zA-Z]*\"')
 '''
 Helper functions
 '''
+def str_to_bool(value):
+    if isinstance(value, bool):
+       return value
+    if value.lower() in ('yes', 'true', 't', 'y', '1'):
+        return True
+    elif value.lower() in ('no', 'false', 'f', 'n', '0'):
+        return False
+    else:
+        raise argparse.ArgumentTypeError('Boolean value expected for overwrite-tables')
+
 def save_to_file(data, ds):
     # Write json response to file
     with open(os.path.join(PATH_TO_VOLUME, f'{ds}.json'),'w') as outfile:
@@ -39,14 +51,6 @@ def remove_wikipedia_refs(data):
                 pass
     return(data)
 
-def flatten_data(data):
-    # Flatten the nested response and do not store Nonetype objects
-    flat_data = []
-    for resp in data:
-        if resp:
-            flat_data += resp
-    return flat_data
-
 def prepare_names(contestants):
     # Preapre contestant names for URL
     return [NN_PATTERN.sub('', name.strip()).replace('  ',' ').replace(' ','_').replace(',','') for name in contestants]
@@ -56,11 +60,15 @@ Collect general information from all seasons of The Bachelor
 https://en.wikipedia.org/wiki/The_Bachelor_(American_TV_series)
 '''
 def scrape1(_):
+    # Initialize sqldb object
+    bachdb = db.bachdb(PATH_TO_DB)
     # Assume the execution of multiple requests, randomize execution time
     time.sleep(random.uniform(3,8))
     resp = ds1.scrape()
     # Clean Wikipedia references from key-value pairs
     resp = remove_wikipedia_refs(resp)
+    # Add documents to ds1 table
+    bachdb.insert_docs('ds1', resp)
     return resp
 
 '''
@@ -68,11 +76,15 @@ Collect general information from all seasons of The Bachelorette
 https://en.wikipedia.org/wiki/The_Bachelorette
 '''
 def scrape2(_):
+    # Initialize sqldb object
+    bachdb = db.bachdb(PATH_TO_DB)
     # Assume the execution of multiple requests, randomize execution time
     time.sleep(random.uniform(3,8))
     resp = ds2.scrape()
     # Clean Wikipedia references from key-value pairs
     resp = remove_wikipedia_refs(resp)
+    # Add documents to ds2 table
+    bachdb.insert_docs('ds2', resp)
     return resp
 
 '''
@@ -80,12 +92,17 @@ Collect general information about all contestants from a given season or all sea
 https://en.wikipedia.org/wiki/The_Bachelor_(season_1)
 '''
 def scrape3(season):
+    # Initialize sqldb object
+    bachdb = db.bachdb(PATH_TO_DB)
     # Assume the execution of multiple requests, randomize execution time
     time.sleep(random.uniform(3,8))
     resp = ds3.scrape_season(season)
-    # Clean Wikipedia references from key-value pairs
+    # Continue if response != None
     if resp:
+        # Clean Wikipedia references from key-value pairs
         resp = remove_wikipedia_refs(resp)
+        # Add documents to ds3 table
+        bachdb.insert_docs('ds3', resp)
     return resp
 
 '''
@@ -93,12 +110,17 @@ Collect general information about all contestants from a given season or all sea
 https://en.wikipedia.org/wiki/The_Bachelorette_(season_1)
 '''
 def scrape4(season):
+    # Initialize sqldb object
+    bachdb = db.bachdb(PATH_TO_DB)
     # Assume the execution of multiple requests, randomize execution time
     time.sleep(random.uniform(3,8))
     resp = ds4.scrape_season(season)
-    # Clean Wikipedia references from key-value pairs
+    # Continue if response != None
     if resp:
+        # Clean Wikipedia references from key-value pairs
         resp = remove_wikipedia_refs(resp)
+        # Add documents to ds4 table
+        bachdb.insert_docs('ds4', resp)
     return resp
 
 '''
@@ -107,15 +129,14 @@ https://bachelor-nation.fandom.com/wiki/Alex_Michel
 '''
 def scrape5(contestant):
     # Initialize sqldb object
-    db = sqldb.db(os.path.join(PATH_TO_VOLUME, 'bach.db'))
+    bachdb = db.bachdb(PATH_TO_DB)
     # Assume the execution of multiple requests, randomize execution time
     time.sleep(random.uniform(3,8))
     resp = ds5.scrape_contestant(contestant)
+    # Continue if response != None
     if resp:
-        # Create a ds5 table if one does not already exist
-        db.create_table('ds5',  db.craft_headers(resp), drop_existing=False)
-        # Add document to ds5 table
-        db.insert_doc('ds5', resp)
+        # Add documents to ds5 table
+        bachdb.insert_doc('ds5', resp)
     del resp # Do this better (handle file output option)
     #return resp
 
@@ -123,141 +144,91 @@ def main():
     # Retrieve args
     parser = argparse.ArgumentParser(description='Process some integers.')
     parser.add_argument('scraper', metavar='N', type=int, nargs='+', help='an integer associated with a data source (i.e. 4)')
-    parser.add_argument('--season', dest='season', type=int, nargs='+', default=[None], help='an integer season (only applicable with data sources 3 and 4) (i.e. 11)')
-    parser.add_argument('--contestant', dest='contestant', type=str, nargs='+', default=[None], help='a string contestant first and last name separated by "_" (only applicable with data source 5) (i.e. joelle_fletcher)')
-    parser.add_argument('--output', dest='output', type=str, choices=['db', 'file'], default='db', help='a string desired output format for collected data: (db or file)')
+    parser.add_argument('--season', dest='season', type=int, nargs='+', default=[], help='an integer season (only applicable with data sources 3 and 4) (i.e. 11)')
+    parser.add_argument('--contestant', dest='contestant', type=str, nargs='+', default=[], help='a string contestant first and last name separated by "_" (only applicable with data source 5) (i.e. joelle_fletcher)')
+    parser.add_argument('--file', dest='file', action='store_true', help='write output data to a file')
+    parser.add_argument('--overwrite', dest='overwrite', action='store_true', help='overwrite applicable table(s) in the database')
     args = parser.parse_args()
 
-    # Specify output format
-    output_format = args.output.lower()
-
     # Initialize sqldb object
-    db = sqldb.db(os.path.join(PATH_TO_VOLUME, 'bach.db'))
+    bachdb = db.bachdb(PATH_TO_DB)
 
     # Initialize multiprocessing pool with 5 threads
     pool = Pool(processes=5)
     # Run asyncronous processes
     if 1 in args.scraper:
+        # Drop and create a new ds1 table, if applicable
+        if args.overwrite:
+            bachdb.create_table('ds1', drop_existing=True)
         ds1_data = pool.map_async(scrape1, [None])
         ds1_resp = ds1_data.get()
-        # Flatten nested mp response
-        ds1_flat = flatten_data(ds1_resp)
-        # Write flattened json response to file, if applicable
-        if output_format == 'file':
-            ds1_out = pool.starmap_async(save_to_file, [[ds1_flat, 'ds1']])
+        # If applicable, write data to file
+        if args.file:
+            ds1_out = pool.starmap_async(save_to_file, [[ds1_resp, 'ds1']])
             ds1_out.get()
-        # Else, insert documents into database
-        else:
-            # Create a ds1 table (drop previously existing if applicable)
-            db.create_table('ds1', db.craft_headers(ds1_flat[0]), drop_existing=True)
-            # Add documents to ds1 table
-            db.insert_docs('ds1', ds1_flat)
     if 2 in args.scraper:
+        # Drop and create a new ds2 table, if applicable
+        if args.overwrite:
+            bachdb.create_table('ds2', drop_existing=True)
         ds2_data = pool.map_async(scrape2, [None])
         ds2_resp = ds2_data.get()
-        # Flatten nested mp response
-        ds2_flat = flatten_data(ds2_resp)
-        # Write flattened json response to file, if applicable
-        if output_format == 'file':
-            ds2_out = pool.starmap_async(save_to_file, [[ds2_flat, 'ds2']])
+        # If applicable, write data to file
+        if args.file:
+            ds2_out = pool.starmap_async(save_to_file, [[ds2_resp, 'ds2']])
             ds2_out.get()
-        # Else, insert documents into database
-        else:
-            # Create a ds2 table (drop previously existing if applicable)
-            db.create_table('ds2',  db.craft_headers(ds2_flat[0]), drop_existing=True)
-            # Add documents to ds2 table
-            db.insert_docs('ds2', ds2_flat)
     if 3 in args.scraper:
-        if args.season == [None]:
-            # Attempt to read data from database
-            max_season = db.get_max_val('ds1','season')
-            # Otherwise, attempt to read data from data source 1 json file
-            if max_season == 0:
-                if os.path.exists(os.path.join(PATH_TO_VOLUME, 'ds1.json')):
-                    with open(os.path.join(PATH_TO_VOLUME, 'ds1.json'),'r') as injson:
-                        general_info = json.load(injson)
-                    max_season = max([rec['season'] for rec in general_info])
-                else:
-                    print('No source for The Bachelor seasons. Please run collection on data source 1. Skipping.')
-        else:
+        # Drop and create a new ds3 table, if applicable
+        if args.overwrite:
+            bachdb.create_table('ds3', drop_existing=True)
+        # If seasons were input, continue with the seasons
+        if len(args.season) > 0:
             seasons = args.season
+        # Else, read seasons from database
+        else:
+            max_season = bachdb.get_max_val('ds1','season')
         if max_season > 0:
             seasons = list(range(1, max_season+1))
             ds3_data = pool.map_async(scrape3, seasons)
             ds3_resp = ds3_data.get()
-            # Flatten nested mp response
-            ds3_flat = flatten_data(ds3_resp)
-            # Write json response to file, if applicable
-            if output_format == 'file':
-                # Do not store Nonetype objects and flatten nested data
-                ds3_out = pool.starmap_async(save_to_file, [[ds3_flat, 'ds3']])
+            # If applicable, write data to file
+            if args.file:
+                ds3_out = pool.starmap_async(save_to_file, [[ds3_resp, 'ds3']])
                 ds3_out.get()
-            # Else, insert documents into database
-            else:
-                # Create a ds3 table (drop previously existing if applicable)
-                db.create_table('ds3',  db.craft_headers(ds3_flat[0]), drop_existing=True)
-                # Add documents to ds3 table
-                db.insert_docs('ds3', ds3_flat)
     if 4 in args.scraper:
-        if args.season == [None]:
-            # Attempt to read data from database
-            max_season = db.get_max_val('ds2','season')
-            # Otherwise, attempt to read data from data source 2 json file
-            if max_season == 0:
-                if os.path.exists(os.path.join(PATH_TO_VOLUME, 'ds2.json')):
-                    with open(os.path.join(PATH_TO_VOLUME, 'ds2.json'),'r') as injson:
-                        general_info = json.load(injson)
-                    max_season = max([rec['season'] for rec in general_info])
-                else:
-                    print('No source for The Bachelorette seasons. Please run collection on data source 2. Skipping.')
-        else:
+        # Drop and create a new ds4 table, if applicable
+        if args.overwrite:
+            bachdb.create_table('ds4', drop_existing=True)
+        # If seasons were input, continue with the seasons
+        if len(args.season) > 0:
             seasons = args.season
+        # Else, read seasons from database
+        else:
+            max_season = bachdb.get_max_val('ds2','season')
         if max_season > 0:
             seasons = list(range(1, max_season+1))
             ds4_data = pool.map_async(scrape4, seasons)
             ds4_resp = ds4_data.get()
-            # Flatten nested mp response
-            ds4_flat = flatten_data(ds4_resp)
-            # Write json response to file, if applicable
-            if output_format == 'file':
-                # Do not store Nonetype objects and flatten nested data
-                ds4_out = pool.starmap_async(save_to_file, [[ds4_flat, 'ds4']])
+            # If applicable, write data to file
+            if args.file:
+                ds4_out = pool.starmap_async(save_to_file, [[ds4_resp, 'ds4']])
                 ds4_out.get()
-            # Else, insert documents into database
-            else:
-                # Create a ds3 table (drop previously existing if applicable)
-                db.create_table('ds4',  db.craft_headers(ds4_flat[0]), drop_existing=True)
-                # Add documents to ds3 table
-                db.insert_docs('ds4', ds4_flat)
     if 5 in args.scraper:
-        if args.contestant == [None]:
-            # Read-in json files of all previously collected contestants
-            contestants = []
-            # Attempt to read data from database
-            contestants += prepare_names([name[0] for name in db.get_docs('ds3', column='name')])
-            contestants += prepare_names([name[0] for name in db.get_docs('ds4', column='name')])
-            # Otherwise, attempt to read data from data sources 3 and 4 files
-            if len(contestants) == 0:
-                # Bachelor contestants
-                if os.path.exists(os.path.join(PATH_TO_VOLUME, 'ds3.json')):
-                    df = pd.read_json(os.path.join(PATH_TO_VOLUME, 'ds3.json'))
-                    contestants += prepare_names(df['name'])
-                else:
-                    print('No source for The Bachelor contestants. Please run collection on data source 3. Skipping.')
-                # Bachelorette contestants
-                if os.path.exists(os.path.join(PATH_TO_VOLUME, 'ds4.json')):
-                    df = pd.read_json(os.path.join(PATH_TO_VOLUME, 'ds4.json'))
-                    contestants += prepare_names(df['name'])
-                else:
-                    print('No source for The Bachelorette contestants. Please run collection on data source 4. Skipping.')
-        else:
+        # Drop and create a new ds5 table, if applicable
+        if args.overwrite:
+            bachdb.create_table('ds5', drop_existing=True)
+        # If seasons were input, continue with the seasons
+        if len(args.contestant) > 0:
             contestants = args.contestant
+        # Else, read contestants from database
+        else:
+            contestants = []
+            contestants += prepare_names([name[0] for name in bachdb.get_docs('ds3', column='name')])
+            contestants += prepare_names([name[0] for name in bachdb.get_docs('ds4', column='name')])
         if len(contestants) > 0:
             ds5_data = pool.map_async(scrape5, contestants)
             ds5_resp = ds5_data.get()
-            # Write json response to file, if applicable
-            if output_format == 'file':
-                # Do not store Nonetype objects and flatten nested data
+            # If applicable, write data to file
+            if args.file:
                 ds5_out = pool.starmap_async(save_to_file, [[ds5_resp, 'ds5']])
                 ds5_out.get()
 
