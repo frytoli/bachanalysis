a975ea700cbcd149dc12012e0c3abbda08249fa9
fryoli <noreply>
2020-11-22 00:09:41 -0600

    added cleaning of wikipedia references

--- scrapers/wikipedia.py
+++ scrapers/wikipedia.py
@@ -4,6 +4,10 @@ from bs4 import BeautifulSoup
 import pandas as pd
 import requests
 import random
+import re
+
+# Global compiled regex pattern for Wikipedia references
+REF_PATTERN = re.compile(r'\[.*\]')
 
 def select_ua():
     user_agents = [
@@ -18,6 +22,19 @@ def select_ua():
         'Mozilla/5.0 (compatible, MSIE 11, Windows NT 6.3; Trident/7.0; rv:11.0) like Gecko'
     ]
     return random.choice(user_agents)
+
+def remove_wikipedia_refs(raw_data):
+    # Replace pairs with references with pairs without references
+    for record in raw_data:
+        for key, value in record.items():
+            if type(value) == str and '[' in value:
+                record[key] = REF_PATTERN.sub('', value)
+            try:
+                record[key] = int(record[key])
+            except ValueError:
+                # Cannot convert value to int
+                pass
+    return(raw_data)
 
 def scrape(show):
     # Select URL
@@ -41,5 +58,7 @@ def scrape(show):
     # Add 'Show' column
     df['Show'] = [show for i in range(len(df.index))]
     # Convert dataframe to dict
-    data = [record for record in df.to_dict(orient='records')]
+    raw_data = [record for record in df.to_dict(orient='records')]
+    # Clean Wikipedia references from key-value pairs
+    data = remove_wikipedia_refs(raw_data)
     return data
